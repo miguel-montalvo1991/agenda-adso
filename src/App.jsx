6828e554e98dae694afe03bb4b24d9287ed7fb65
Miguel-Montalvo <luismiguelmontalvoalvarez@gmail.com>
2026-02-16 21:59:21 -0500

    chore: configuración inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  const fecha = new Date().toLocaleString();
+
+  return (
+    <main>
+      <h1>HOLA SOY MIGUEL, APRENDIZ DEL SENA </h1>
+      <p>{fecha}</p>
+    </main>
+  );
+}
