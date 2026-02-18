7da3614291e5beace09e79bad0988e5d3e1cfd41
Miguel-Montalvo <luismiguelmontalvoalvarez@gmail.com>
2026-02-18 16:48:45 -0500

    feat: Clase 5 - persistencia con localStorage Agenda ADSO v3

--- src/components/ContactoCard.jsx
+++ src/components/ContactoCard.jsx
@@ -1,24 +1,12 @@
-export default function ContactoCard({
-  id,
-  nombre,
-  telefono,
-  correo,
-  etiqueta,
-  onDelete,
-}) {
+export default function ContactoCard({ nombre, telefono, correo, etiqueta, onEliminar }) {
   return (
     <article className="tarjeta-contacto">
       <h3>{nombre}</h3>
-      {etiqueta && <p className="tag">{etiqueta}</p>}
       <p>📞 {telefono}</p>
-      {correo && <p>✉️ {correo}</p>}
-
+      <p>✉️ {correo}</p>
+      {etiqueta && <p>{etiqueta}</p>}
       <div className="acciones">
-        <button
-          type="button"
-          className="btn-eliminar"
-          onClick={() => onDelete(id)}
-        >
+        <button className="btn-eliminar" onClick={() => onEliminar(correo)}>
           Eliminar
         </button>
       </div>
